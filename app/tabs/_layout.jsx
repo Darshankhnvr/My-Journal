b36ebda651c5dfa5905be02e75338a623b85cc48
Darshan <darshankgoudahnvr2020@gmail.com>
2025-04-22 07:11:22 +0530

    2nd test

--- app/tabs/_layout.jsx
+++ app/tabs/_layout.jsx
@@ -6,10 +6,10 @@ export default function TabsLayout() {
   return (
     <Tabs>
       <Tabs.Screen name="index" options={{ title: 'Journal', tabBarIcon: ({ color, focused }) => (
-            <FontAwesome5 name="journal-whills" size={25} color="black" />
+            <FontAwesome5 name="journal-whills" size={24} color="black" />
           ), }} />
       <Tabs.Screen name="explore" options={{ title: 'Explore',  tabBarIcon: ({ color, focused }) => (
-            <FontAwesome name="search" size={25} color="black" />
+            <FontAwesome name="search" size={24} color="black" />
           ) }} />
     </Tabs>
   );
